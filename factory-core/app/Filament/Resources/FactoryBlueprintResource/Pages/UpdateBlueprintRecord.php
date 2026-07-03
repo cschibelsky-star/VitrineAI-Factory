fed3f58e27ed4ab13372f18c73e5a1ea291b77fe
cschibelsky-star <cschibelsky@gmail.com>
2026-07-03 15:05:43 -0300

    factory-core: add blueprint update page

---
+++ factory-core/app/Filament/Resources/FactoryBlueprintResource/Pages/UpdateBlueprintRecord.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\FactoryBlueprintResource\Pages;
+
+use App\Filament\Resources\FactoryBlueprintResource;
+use Filament\Resources\Pages\EditRecord;
+
+class UpdateBlueprintRecord extends EditRecord
+{
+    protected static string $resource = FactoryBlueprintResource::class;
+}
