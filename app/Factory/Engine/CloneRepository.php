b92b48aca3d74076e02044908de9e793bc069468
Cristian Schibelsky <cris1649@tvsumare.com.br>
2026-07-04 17:33:04 -0300

    BUILD 4.6 melhora log de clone GitHub

--- app/Factory/Engine/CloneRepository.php
+++ app/Factory/Engine/CloneRepository.php
@@ -6,6 +6,8 @@ use Symfony\Component\Process\Process;
 
 class CloneRepository
 {
+    public string $error = '';
+
     public function execute(string $repository, string $branch, string $destination): bool
     {
         if (is_dir($destination)) {
@@ -17,12 +19,14 @@ class CloneRepository
             'clone',
             '--branch',
             $branch,
-            "https://github.com/$repository.git",
+            "git@github.com:$repository.git",
             $destination,
         ]);
 
         $process->setTimeout(1800);
         $process->run();
+
+        $this->error = $process->getErrorOutput() ?: $process->getOutput();
 
         return $process->isSuccessful();
     }
