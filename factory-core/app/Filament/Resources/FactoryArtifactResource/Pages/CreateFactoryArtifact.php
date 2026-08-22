f2b8773df15f870738e9ed94a664b97772887e17
cschibelsky-star <cschibelsky@gmail.com>
2026-08-22 13:49:13 -0300

    Add artifact create page

---
+++ factory-core/app/Filament/Resources/FactoryArtifactResource/Pages/CreateFactoryArtifact.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\FactoryArtifactResource\Pages;
+
+use App\Filament\Resources\FactoryArtifactResource;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateFactoryArtifact extends CreateRecord
+{
+    protected static string $resource = FactoryArtifactResource::class;
+}
