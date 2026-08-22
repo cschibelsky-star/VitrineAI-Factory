97dc65b4c495f336fb1752493cf833aa2a5d317f
cschibelsky-star <cschibelsky@gmail.com>
2026-08-22 13:48:50 -0300

    Add build create page

---
+++ factory-core/app/Filament/Resources/FactoryBuildResource/Pages/CreateFactoryBuild.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\FactoryBuildResource\Pages;
+
+use App\Filament\Resources\FactoryBuildResource;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateFactoryBuild extends CreateRecord
+{
+    protected static string $resource = FactoryBuildResource::class;
+}
