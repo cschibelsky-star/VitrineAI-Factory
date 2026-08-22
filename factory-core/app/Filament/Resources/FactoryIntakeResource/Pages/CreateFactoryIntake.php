62ae5624020029c023dc876035198bf815c34645
cschibelsky-star <cschibelsky@gmail.com>
2026-08-22 13:42:51 -0300

    Add Factory intake create page

---
+++ factory-core/app/Filament/Resources/FactoryIntakeResource/Pages/CreateFactoryIntake.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\FactoryIntakeResource\Pages;
+
+use App\Filament\Resources\FactoryIntakeResource;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateFactoryIntake extends CreateRecord
+{
+    protected static string $resource = FactoryIntakeResource::class;
+}
