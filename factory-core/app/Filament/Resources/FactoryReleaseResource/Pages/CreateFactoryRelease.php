ea25af47a818c6c3003f315685a2f96fe484218c
cschibelsky-star <cschibelsky@gmail.com>
2026-08-22 13:51:44 -0300

    Add Factory releases create page

---
+++ factory-core/app/Filament/Resources/FactoryReleaseResource/Pages/CreateFactoryRelease.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\FactoryReleaseResource\Pages;
+
+use App\Filament\Resources\FactoryReleaseResource;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateFactoryRelease extends CreateRecord
+{
+    protected static string $resource = FactoryReleaseResource::class;
+}
