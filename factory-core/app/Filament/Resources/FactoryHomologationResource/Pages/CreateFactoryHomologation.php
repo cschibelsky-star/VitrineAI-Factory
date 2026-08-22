a47657ed69fe1ed451d59921c03bd1d948ffd9b1
cschibelsky-star <cschibelsky@gmail.com>
2026-08-22 13:51:21 -0300

    Add Factory homologation create page

---
+++ factory-core/app/Filament/Resources/FactoryHomologationResource/Pages/CreateFactoryHomologation.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\FactoryHomologationResource\Pages;
+
+use App\Filament\Resources\FactoryHomologationResource;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateFactoryHomologation extends CreateRecord
+{
+    protected static string $resource = FactoryHomologationResource::class;
+}
