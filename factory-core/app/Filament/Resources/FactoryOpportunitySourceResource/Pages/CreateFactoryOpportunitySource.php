b4e8eae5434920a55f6c253b10c6e56a31226152
cschibelsky-star <cschibelsky@gmail.com>
2026-08-23 11:21:06 -0300

    Add opportunity source create page

---
+++ factory-core/app/Filament/Resources/FactoryOpportunitySourceResource/Pages/CreateFactoryOpportunitySource.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\FactoryOpportunitySourceResource\Pages;
+
+use App\Filament\Resources\FactoryOpportunitySourceResource;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateFactoryOpportunitySource extends CreateRecord
+{
+    protected static string $resource = FactoryOpportunitySourceResource::class;
+}
