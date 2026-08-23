e04e516747a87343b64688b18579e5359e9d2669
cschibelsky-star <cschibelsky@gmail.com>
2026-08-23 12:28:50 -0300

    Add opportunity action create page

---
+++ factory-core/app/Filament/Resources/FactoryOpportunityActionResource/Pages/CreateFactoryOpportunityAction.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\FactoryOpportunityActionResource\Pages;
+
+use App\Filament\Resources\FactoryOpportunityActionResource;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateFactoryOpportunityAction extends CreateRecord
+{
+    protected static string $resource = FactoryOpportunityActionResource::class;
+}
